5c8398043b0d3b23900158988a445031b0b446df
AttAditya <attachment.aditya@gmail.com>
2025-12-17 00:03:26 +0530

    Handsome Romeo and Prettier Juliet

--- src/config/renderer.ts
+++ src/config/renderer.ts
@@ -2,8 +2,8 @@ import { COLORS } from "@config/colors";
 
 export const RENDERER_CONFIG = {
   backgroundColor: COLORS.BACKGROUND.SKY,
-  antialias: false,
-  resolution: 1,
-  roundPixels: false,
+  antialias: true,
+  resolution: 4,
+  roundPixels: true,
 };
 
